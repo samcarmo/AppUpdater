447e66479ab4eedcd15075ff931d3ac0eaaaac37
samuelcarmo <samuelcarmo_2@hotmail.com>
2022-11-26 10:33:36 -0400

    first commit

---
+++ src/model/dao/WingetDAO.java
@@ -0,0 +1,63 @@
+package model.dao;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author samue
+ */
+public class WingetDAO {
+
+    public String command;
+    public String info;
+
+    public void runCommand(String command) throws Exception {
+        try {
+            ProcessBuilder builder = new ProcessBuilder(
+                    "cmd.exe", "/c", command);
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while (true) {
+                line = r.readLine();
+                if (line == null) {
+                    break;
+                }
+                System.out.println(line);
+                info = line;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(WingetDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getUpdate() throws Exception {
+        command = "winget upgrade all";
+        this.runCommand(command);
+        return info;
+    }
+
+    public String getOutdated() throws Exception {
+        command = "winget upgrade";
+        this.runCommand(command);
+        return info;
+    }
+
+    public String getAppList() throws Exception {
+        command = "winget export -o apps.json";
+        this.runCommand(command);
+        return info;
+    }
+
+}
